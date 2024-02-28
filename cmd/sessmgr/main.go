03ac91ffce399966404af4afa07b4858a6994284
ionrock <eric@ionrock.org>
2024-02-28 14:37:06 -0600

    added add session cli

--- cmd/sessmgr/main.go
+++ cmd/sessmgr/main.go
@@ -2,52 +2,109 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
+	"os"
 
 	userup "github.com/hillside-labs/userservice-go-sdk/go-client"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	client, err := userup.NewClient("localhost:9000")
-	if err != nil {
-		log.Fatal(err)
+
+	app := cli.App{
+		Name:  "sessmgr",
+		Usage: "Inspect and work with Anonymous Sessions.",
+		Action: func(c *cli.Context) error {
+			client, err := userup.NewClient("localhost:9000")
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer client.Close()
+
+			fmt.Println("Created a connection")
+
+			sessID := userup.NewSessionID()
+			err = client.AddSession(context.Background(), sessID, map[string]interface{}{"hello": "world"})
+			if err != nil {
+				fmt.Println("Error creating a session")
+				log.Fatal(err)
+			}
+
+			fmt.Println("Created a new session with session id ", sessID)
+
+			sl := userup.NewSessionLogger(userup.SessionEventLoggerConfig(userup.NewSessionLoggerConfig("io.userup.test.sessmgr", client)))
+
+			fmt.Println("New session logger created")
+
+			err = sl.LogEvent(context.Background(), sessID, "application/json", "test_schema", "view_cart", map[string]interface{}{"url": "/greatjeans/2/blue"})
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			fmt.Println("Sent a session event")
+
+			client.AddUserToSession(context.Background(), sessID, uint64(123))
+			fmt.Println("Added session", sessID, "to user 123")
+
+			return nil
+		},
+		Commands: []*cli.Command{
+			{
+				Name:  "add",
+				Usage: "Add a new session.",
+				Flags: []cli.Flag{
+					&cli.BoolFlag{
+						Name:    "generate",
+						Aliases: []string{"g"},
+					},
+					&cli.StringFlag{
+						Name:    "json",
+						Aliases: []string{"j"},
+					},
+				},
+				Action: func(c *cli.Context) error {
+					client, err := userup.NewClient("localhost:9000")
+					if err != nil {
+						log.Fatal(err)
+					}
+					defer client.Close()
+
+					sessID := c.Args().First()
+					if c.Bool("generate") {
+						sessID = userup.NewSessionID()
+					}
+
+					doc := make(map[string]interface{})
+					json.Unmarshal([]byte(c.String("json")), &doc)
+
+					err = client.AddSession(context.Background(), sessID, doc)
+					if err != nil {
+						fmt.Println("Error creating a session")
+						log.Fatal(err)
+					}
+
+					return nil
+				},
+			},
+			{
+				Name:  "ls",
+				Usage: "List our existing sessions.",
+			},
+			{
+				Name:  "get",
+				Usage: "Get a specific session's events.",
+			},
+
+			{
+				Name:  "identify",
+				Usage: "Identify a session as belonging to a specific user.",
+			},
+		},
 	}
-	defer client.Close()
 
-	fmt.Println("Created a connection")
-
-	sessID := userup.NewSessionID()
-	err = client.AddSession(context.Background(), sessID, map[string]interface{}{"hello": "world"})
-	if err != nil {
-		fmt.Println("Error creating a session")
+	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
-	}
-
-	fmt.Println("Created a new session with session id ", sessID)
-
-	sl := userup.NewSessionLogger(userup.SessionEventLoggerConfig(userup.NewSessionLoggerConfig("io.userup.test.sessmgr", client)))
-
-	fmt.Println("New session logger created")
-
-	err = sl.LogEvent(context.Background(), sessID, "application/json", "test_schema", "view_cart", map[string]interface{}{"url": "/greatjeans/2/blue"})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Sent a session event")
-
-	client.IdentifySession(context.Background(), sessID, uint64(123))
-	fmt.Println("Added session", sessID, "to user 123")
-
-	sessions, err := client.GetSessions(context.Background(), &userup.SessionQuery{
-		UserID: uint64(123),
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Got sessions for user 123")
-	for _, session := range sessions {
-		fmt.Printf("%+v\n", session)
 	}
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,15 @@ module github.com/hillside-labs/userservice-go-sdk
 go 1.22.0
 
 require (
+	github.com/urfave/cli/v2 v2.27.1
 	google.golang.org/grpc v1.61.0
 	google.golang.org/protobuf v1.32.0
+)
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 )
 
 require (
