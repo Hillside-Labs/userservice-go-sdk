4f98092bc4f822c18fc18730827de18f4db208b8
ionrock <eric@ionrock.org>
2024-02-28 22:27:06 -0600

    Finished up the sessmgr cli
    We can ls sessions, get a session to see it's events, and identify a
    session to a user.

--- cmd/sessmgr/main.go
+++ cmd/sessmgr/main.go
@@ -6,8 +6,11 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 
+	"github.com/fatih/color"
 	userup "github.com/hillside-labs/userservice-go-sdk/go-client"
+	"github.com/rodaine/table"
 	"github.com/urfave/cli/v2"
 )
 
@@ -16,40 +19,6 @@ func main() {
 	app := cli.App{
 		Name:  "sessmgr",
 		Usage: "Inspect and work with Anonymous Sessions.",
-		Action: func(c *cli.Context) error {
-			client, err := userup.NewClient("localhost:9000")
-			if err != nil {
-				log.Fatal(err)
-			}
-			defer client.Close()
-
-			fmt.Println("Created a connection")
-
-			sessID := userup.NewSessionID()
-			err = client.AddSession(context.Background(), sessID, map[string]interface{}{"hello": "world"})
-			if err != nil {
-				fmt.Println("Error creating a session")
-				log.Fatal(err)
-			}
-
-			fmt.Println("Created a new session with session id ", sessID)
-
-			sl := userup.NewSessionLogger(userup.SessionEventLoggerConfig(userup.NewSessionLoggerConfig("io.userup.test.sessmgr", client)))
-
-			fmt.Println("New session logger created")
-
-			err = sl.LogEvent(context.Background(), sessID, "application/json", "test_schema", "view_cart", map[string]interface{}{"url": "/greatjeans/2/blue"})
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			fmt.Println("Sent a session event")
-
-			client.AddUserToSession(context.Background(), sessID, uint64(123))
-			fmt.Println("Added session", sessID, "to user 123")
-
-			return nil
-		},
 		Commands: []*cli.Command{
 			{
 				Name:  "add",
@@ -85,21 +54,114 @@ func main() {
 						log.Fatal(err)
 					}
 
+					fmt.Println("session created: ", sessID)
+					out, _ := json.MarshalIndent(doc, "", "  ")
+					fmt.Println(string(out))
+
 					return nil
 				},
 			},
 			{
 				Name:  "ls",
 				Usage: "List our existing sessions.",
+				Action: func(c *cli.Context) error {
+					client, err := userup.NewClient("localhost:9000")
+					if err != nil {
+						log.Fatal(err)
+					}
+					defer client.Close()
+
+					sessions, err := client.GetSessions(context.Background(), &userup.SessionQuery{
+						Limit: 100,
+					})
+					if err != nil {
+						log.Fatal(err)
+					}
+
+					if len(sessions) == 0 {
+						return nil
+					}
+
+					headerFmt := color.New(color.FgGreen, color.Underline).SprintfFunc()
+					columnFmt := color.New(color.FgHiBlue, color.Bold).SprintfFunc()
+
+					tbl := table.New("Session Key", "User ID", "Object")
+					tbl.WithHeaderFormatter(headerFmt).WithFirstColumnFormatter(columnFmt)
+
+					for _, sess := range sessions {
+
+						objJSON, _ := json.MarshalIndent(sess.Object.AsMap(), "", "  ")
+						tbl.AddRow(sess.Key, sess.UserId, string(objJSON))
+					}
+
+					tbl.Print()
+
+					return nil
+				},
 			},
 			{
 				Name:  "get",
 				Usage: "Get a specific session's events.",
+				Action: func(c *cli.Context) error {
+					client, err := userup.NewClient("localhost:9000")
+					if err != nil {
+						log.Fatal(err)
+					}
+					defer client.Close()
+
+					sessID := c.Args().First()
+					if sessID == "" {
+						return fmt.Errorf("Please provide a Session ID argument.")
+					}
+
+					events, err := client.GetSessionEvents(context.Background(), &userup.SessionEventQuery{
+						SessionKeys: []string{sessID},
+					})
+
+					if err != nil {
+						log.Fatal(err)
+					}
+
+					headerFmt := color.New(color.FgGreen, color.Underline).SprintfFunc()
+					columnFmt := color.New(color.FgHiBlue, color.Bold).SprintfFunc()
+
+					tbl := table.New("Subject", "Type", "User ID", "Object")
+					tbl.WithHeaderFormatter(headerFmt).WithFirstColumnFormatter(columnFmt)
+
+					for _, event := range events {
+						tbl.AddRow(event.Subject, event.Type, event.UserId, string(event.Data))
+					}
+
+					tbl.Print()
+
+					return nil
+				},
 			},
 
 			{
 				Name:  "identify",
 				Usage: "Identify a session as belonging to a specific user.",
+				Action: func(c *cli.Context) error {
+					client, err := userup.NewClient("localhost:9000")
+					if err != nil {
+						log.Fatal(err)
+					}
+					defer client.Close()
+
+					sessID := c.Args().First()
+					userIDstr := c.Args().Get(2)
+
+					if sessID == "" || userIDstr == "" {
+						return fmt.Errorf("Missing args: SESSION_ID USER_ID")
+					}
+
+					userID, err := strconv.ParseUint(userIDstr, 10, 64)
+					if err != nil {
+						return err
+					}
+
+					return client.IdentifySession(context.Background(), sessID, userID)
+				},
 			},
 		},
 	}
